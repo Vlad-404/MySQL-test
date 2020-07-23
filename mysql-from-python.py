418fae8193eeb5e9f63fd0aab44ded3b5fd0f4b6
Vlad-404 <vmijat21@gmail.com>
2020-07-23 19:54:44 +0000

    finished unit

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -14,8 +14,10 @@ connection = pymysql.connect(host='localhost',
 try:
     # Run a query
     with connection.cursor() as cursor:
-        row = ("Bob", 21, "1990-02-06 23:04:56")
-        cursor.execute("INSERT INTO Friends VALUES (%s, %s, %s);", row)
+        list_of_names = ['Fred']
+        #Prepare string with the same number of placeholders as in list_of_names
+        format_strings = ','.join(['%s']*len(list_of_names))
+        cursor.execute("DELETE FROM Friends WHERE name in ({});".format(format_strings), list_of_names)
         connection.commit()
 finally:
     # Close the connection, regardless of whether the above was succesfull
